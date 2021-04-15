8355bde97ba29407e1be25eed76d7391920aae1f
644 <17060633+644@users.noreply.github.com>
2021-04-15 14:15:36 +0100

    Update iq-solve.php

--- iq-solve.php
+++ iq-solve.php
@@ -5,10 +5,11 @@ header('Content-type: application/json');
 
 $cdata = $_POST['captchadata'];
 $formula = $_POST['formula'];
+$num = $_POST['num'];
 file_put_contents('tmp.jpg', file_get_contents($cdata));
 file_put_contents('formula.txt', $formula);
 
-$cmd_res = shell_exec('/path/to/get-solution');
+$cmd_res = shell_exec('/path/to/get-solution "'.$num.'"');
 $res = array('solution' => $cmd_res);
 
 echo json_encode($res);
